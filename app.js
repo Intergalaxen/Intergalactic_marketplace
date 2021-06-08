016978a0d72862cdfbc09cf0d9581307e36d89cd
Intergalaxen <westbergalfrida@gmail.com>
2021-06-08 21:36:05 +0200

    fixed a layout bug

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const port = 5000;
 
 // Static Files
 app.use(express.static("public"));
-// Example for other folders - not required
 app.use("/css", express.static(__dirname + "public/css"));
 
 // Set Templating Engine
