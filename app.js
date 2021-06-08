5383728df9b2c2e4122559df18d116889aafc9ba
Intergalaxen <westbergalfrida@gmail.com>
2021-06-08 20:27:12 +0200

    added a footer and a header

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const port = 5000;
 // Static Files
 app.use(express.static("public"));
 // Example for other folders - not required
-// app.use('/css', express.static(__dirname + 'public/css'))
+app.use("/css", express.static(__dirname + "public/css"));
 
 // Set Templating Engine
 app.use(expressLayouts);
-//app.set("layout", "./layouts/full-width");
+app.set("layout", "./layouts/full-width");
 app.set("view engine", "ejs");
 
 // Routes
