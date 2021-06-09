6e6cbe1928cbede2a42ccf22b732d3ff2abdfbb3
Intergalaxen <westbergalfrida@gmail.com>
2021-06-09 15:12:33 +0200

    added a contact and a signup page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = 5000;
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/js", express.static(__dirname + "public/js"));
+app.use("/images", express.static(__dirname + "public/images"));
 
 // Set Templating Engine
 app.use(expressLayouts);
@@ -20,12 +21,24 @@ app.get("", (req, res) => {
     res.render("index", { title: "Home Page" });
 });
 
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About Page", layout: "./layouts/sidebar" });
+});
+
 app.get("/marketplace", (req, res) => {
     res.render("marketplace", { title: "Marketplace" });
 });
 
-app.get("/about", (req, res) => {
-    res.render("about", { title: "About Page", layout: "./layouts/sidebar" });
+app.get("/login-signup", (req, res) => {
+    res.render("login-signup", { title: "Login or signup" });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact", { title: "Contact us" });
+});
+
+app.get("/checkout", (req, res) => {
+    res.render("checkout", { title: "Checkout" });
 });
 
 // Listen on Port 5000
