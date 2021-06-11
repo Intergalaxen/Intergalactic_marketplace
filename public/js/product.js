9768495c91e6947817ab0e0c085395eda6a9a5ed
Intergalaxen <westbergalfrida@gmail.com>
2021-06-11 13:27:01 +0200

    added cookies and a cart

---
+++ public/js/product.js
@@ -0,0 +1,28 @@
+function updateCart() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "/cartnum?user=" + getCookie("user"));
+    xhr.onreadystatechange = () => {
+        if (xhr.status = XMLHttpRequest.DONE) {
+            document.getElementById("cart").innerText = "Varukorg: " + xhr.response;
+        }
+    }
+    xhr.send();
+}
+
+function buy(id) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "/buy?id=" + id + "&user=" + getCookie("user"));
+    xhr.onreadystatechange = () => {
+        if (xhr.status = XMLHttpRequest.DONE) {
+            updateCart();
+        }
+    }
+    xhr.send();
+}
+
+// Uppdatera cart-delen direkt
+updateCart();
+
+window.onload = () => {
+    document.getElementById("cart").href += "?user=" + getCookie("user");
+}
